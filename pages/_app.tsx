2ebe908dc0b2811bba2dc3e3ba7ad11ba5b6e4ad
Shalva Stephniashvili <shalvastep@gmail.com>
2022-11-23 09:10:55 -0500

    changed menubar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,25 @@ import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import '/node_modules/primeflex/primeflex.css';
+import { Menubar } from 'primereact/menubar';
+import config from 'utils/config';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const items: any[] = [
+		{
+			label: 'Home',
+			icon: 'pi pi-fw pi-power-off',
+			url: `${config.host}`
+			// command: () => {
+			// 	router.push({ pathname: '/index' });
+			// }
+		}
+	];
+	return (
+		<>
+			{' '}
+			<Menubar model={items} />
+			<Component {...pageProps} />
+		</>
+	);
 }
